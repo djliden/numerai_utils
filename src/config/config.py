13e5dbcfdb79f4cd1fca2225520ee4274005734c
dliden <dliden@protonmail.com>
2021-03-15 22:43:03 -0700

    Add prototype Configuration class
    This class is not yet ready for use. I am still working on a way to
    access the dictionary items in the configuration as class instance
    attributes. I think this should be fine if I disallow adding new
    configuration options via the class object (rather than its methods).

--- src/config/config.py
+++ src/config/config.py
@@ -1,35 +1,124 @@
 from os import PathLike
 import yaml
 
-class Configuration:
-    def __init__(self,
-                 default_cfg:PathLike,
-                 model_cfg:PathLike,
-                 user_cfg:PathLike = None):
-        """Class for accessing and updating configuration files
+"""
+An important note: At this phase, the classes contained here muddy the
+relationship between the class instance's __dict__ and the stored
+configuration dictionaries (e.g. Configuration.config). This could cause
+problems if one were to try to *update* the configuration by defining a
+class attribute, e.g. Configuration.Model.lr = 0.1. I see the most potential for this to be problematic in the log output -- anything added in this way would
+not be included in the .config or .log dictionary and thus would not be
+included in the output. I hope to find a good way to make both approaches
+work, but in the meantime, to be safe, it is better to *add* to the config
+only using the functions designed for that purpose. Accessing parts of the
+config through class attributes should currently be acceptable, though.
+
+Maybe something from here would help https://docs.python.org/3/reference/datamodel.html?emulating-container-types#emulating-container-types
+
+Or here https://stackoverflow.com/questions/38034377/object-like-attribute-access-for-nested-dictionary
+
+From SE:
+
+class AttrDict(dict):
+"" Dictionary subclass whose entries can be accessed by attributes
+    (as well as normally).
+''
+def __init__(self, *args, **kwargs):
+    def from_nested_dict(data):
+        "" Construct nested AttrDicts from nested dictionaries. ""
+        if not isinstance(data, dict):
+            return data
+        else:
+            return AttrDict({key: from_nested_dict(data[key])
+                                for key in data})
+
+    super(AttrDict, self).__init__(*args, **kwargs)
+    self.__dict__ = self
+
+    for key in self.keys():
+        self[key] = from_nested_dict(self[key])
+
+Or I can just copy YACS https://github.com/rbgirshick/yacs/blob/32d5e4ac300eca6cd3b839097dde39c4017a1070/yacs/config.py#L112
+
+The challenge is with class attribute <-> dict interoperability.
+
+"""
+
+class Configuration(dict):
+    """Class for accessing and updating configuration files
+    
+        kwargs:
+        * config: path to an initial configuration file
+        """
+    def __init__(self, config:PathLike = None):
+        self.config = dict()
+        if config:
+            config = yaml.load(open(config, 'r'), Loader = yaml.SafeLoader)
+            self.config.update(config)
+        self.__dict__.update(self.config)
+        
+    def update_from_yaml(self, new_config):
+        """Update the stored configuration
 
         kwargs:
-        * default_cfg: path to default configutation file
-        * model_cfg: path to model-default configuration file
-        * user_cfg: path to user-specified configuration file
+        * new_config: path to YAML file
+
+        If new_config includes a key/value pair already included in the
+        existing config, the existing config will be overwritten with
+        the new value. If new_config includes new key/value pairs, they
+        will be added to the existing config.
         """
-        self.def = yaml.load(open(default_cfg, 'r'), Loader = yaml.CLoader)
-        self.model = yaml.load(open(model_cfg, 'r'), Loader = yaml.CLoader)
-        if user_cfg:
-            self.user = yaml.load(open(user_cfg, 'r'), Loader = yaml.CLoader)
-
-    def merge_configs(self):
-        self.config = dict()
-        self.config.update(self.def)
-        self.config.update(self.model)
-        if user_cfg: self.config.update(self.user)
-
-    def init_config(self):
-        self.merge_configs()
+        new_config = yaml.load(open(new_config, 'r'), Loader = yaml.SafeLoader)
+        self.config.update(new_config)
         self.__dict__.update(self.config)
+        
+    def update_from_dict(self, new_config:dict):
+        """Update the stored configuration
+
+        kwargs:
+        * new_config: python dict
+
+        If new_config includes a key/value pair already included in the
+        existing config, the existing config will be overwritten with
+        the new value. If new_config includes new key/value pairs, they
+        will be added to the existing config.
+        """
+        self.config.update(new_config)    
+        self.__dict__.update(self.config)
+
+    def print_config(self):
+        print(yaml.dump(self.config))
+
     
 class Log:
-    def __init__(self, config):
-        self.config = config
+    """Class for logging and returning output of model runs
+
+    kwargs:
+    * include_config: whether the configuration should be included in
+      the log output
+    * config (dict): the configuration to be included, if include_config
+      is True. Note that this argument expects a dictionary, so from an
+      instance of the Configuration class, pass Configuration.config to
+      the log class.
+    """
+    def __init__(self, config:dict, incude_config:bool = True):
+        self.log = dict()
+        if include_config:
+            self.config = config
+            self.log.update(self.config)
+        self.__dict__.update(self.log)
     
     
+    def update_from_dict(self, new_log:dict):
+        """Update the stored configuration
+
+        kwargs:
+        * new_log: python dict
+
+        If new_log includes a key/value pair already included in the
+        existing log, the existing log will be overwritten with
+        the new value. If new_log includes new key/value pairs, they
+        will be added to the existing log.
+        """
+        self.log.update(new_log)    
+        self.__dict__.update(self.log)
