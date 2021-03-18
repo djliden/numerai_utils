d9ca81ddfeaceb254c945e8693e7ca12d545af52
dliden <dliden@protonmail.com>
2021-03-18 09:14:50 -0700

    Add notebook exploring new config setup and delete old config
    The old config system is no longer in use, and this package no longer
    depends on YACS.
    
    The new config system, though functional, is still a work in
    progress.

--- src/config/config.py
+++ src/config/config.py
@@ -1,7 +1,83 @@
 import yaml
 import textwrap
 
-class Config():
+class BaseConfig:
+    __isFrozen = True
+    def __init__(self):
+        self._thaw()
+        self.A = "This is from the parent class"
+        self._freeze()
+        
+    def __setattr__(self, key, value):
+        """Prevent directly setting attributes
+
+        This is to ensure that all configuration options are defined
+        directly through the dictionary or YAML adding methods. This
+        limitation could be lifted in the future.
+        """
+        if self.__isFrozen and not hasattr(self, key):
+            raise TypeError(textwrap.fill(textwrap.dedent("""
+            Directly adding new attributes to a Configuration 
+            object using dot notation is not supported. Please use 
+            the update_config() method""")))
+        object.__setattr__(self, key, value)
+        
+    def _freeze(self):
+        """Prevent changes to the __dict__"""
+        self.__isFrozen = True
+        
+    def _thaw(self):
+        """Allow changes to the __dict__"""
+        self.__isFrozen = False
+    
+    def _update_state(self):
+        for key in self.config.keys():
+            if isinstance(self.config[key], dict):
+                self.__setattr__(key, Config(self.config[key]))
+
+class ConfigI(BaseConfig):
+    __isFrozen = True
+    def __init__(self, config):
+        super().__init__()
+        self._thaw()
+        self.config = dict()
+        if isinstance(config, dict):
+            self.update_from_dict(config)
+        else:
+            self.update_from_yaml(config)
+        self._update_state()
+        self._freeze()
+    def update_from_yaml(self, new_config):
+        """Update the stored configuration
+
+        kwargs:
+        * new_config: path to YAML file
+
+        If new_config includes a key/value pair already included in the
+        existing config, the existing config will be overwritten with
+        the new value. If new_config includes new key/value pairs, they
+        will be added to the existing config.
+        """
+        new_config = yaml.load(open(new_config, 'r'), Loader = yaml.SafeLoader)
+        self.config.update(new_config)
+        self.__dict__.update(self.config)
+        
+    def update_from_dict(self, new_config:dict):
+        """Update the stored configuration
+
+        kwargs:
+        * new_config: python dict
+
+        If new_config includes a key/value pair already included in the
+        existing config, the existing config will be overwritten with
+        the new value. If new_config includes new key/value pairs, they
+        will be added to the existing config.
+        """
+        self.config.update(new_config)    
+        self.__dict__.update(new_config)
+    
+
+class Config:
     __isFrozen = True
     def __init__(self, config):
         self._thaw()
