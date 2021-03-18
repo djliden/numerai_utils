fc5e93df7f8ac1199af5612d9afa35f6342b2ddd
dliden <dliden@protonmail.com>
2021-03-18 11:33:45 -0700

    Add config printing and config type checking to Config class
    In an effort to encourage future extensibility, I expanded the
    configuration checker to check if the input is a dict or yaml and then
    to return a TypeError noting that one of these two inputs is
    needed. It will be fairly trivial to add, e.g., JSON support in the
    future.
    
    I also added a method to print and optionally save the config file to
    a specified directory.
    
    I deleted the deprecated code.
    
    With this, the config system has all of the functionality we need.

--- src/config/config.py
+++ src/config/config.py
@@ -1,13 +1,11 @@
+from os.path import splitext
 import yaml
 import textwrap
 
 class BaseConfig:
     __isFrozen = True
     def __init__(self):
-        self._thaw()
-        self.A = "This is from the parent class"
-        self._freeze()
-        
+        self.__isFrozen = True
     def __setattr__(self, key, value):
         """Prevent directly setting attributes
 
@@ -34,19 +32,7 @@ class BaseConfig:
         for key in self.config.keys():
             if isinstance(self.config[key], dict):
                 self.__setattr__(key, Config(self.config[key]))
-
-class ConfigI(BaseConfig):
-    __isFrozen = True
-    def __init__(self, config):
-        super().__init__()
-        self._thaw()
-        self.config = dict()
-        if isinstance(config, dict):
-            self.update_from_dict(config)
-        else:
-            self.update_from_yaml(config)
-        self._update_state()
-        self._freeze()
+                
     def update_from_yaml(self, new_config):
         """Update the stored configuration
 
@@ -75,82 +61,38 @@ class ConfigI(BaseConfig):
         """
         self.config.update(new_config)    
         self.__dict__.update(new_config)
-    
 
-class Config:
-    __isFrozen = True
+class Config(BaseConfig):
     def __init__(self, config):
+        #self._isFrozen = True
+        super().__init__()
         self._thaw()
         self.config = dict()
-        if isinstance(config, dict):
-            self.update_from_dict(config)
-        else:
-            self.update_from_yaml(config)
-        self._update_state()
+        self.update_config(config)
         self._freeze()
         
-    def _freeze(self):
-        """Prevent changes to the __dict__"""
-        self.__isFrozen = True
-        
-    def _thaw(self):
-        """Allow changes to the __dict__"""
-        self.__isFrozen = False
-    
-    def _update_state(self):
-        for key in self.config.keys():
-            if isinstance(self.config[key], dict):
-                self.__setattr__(key, Config(self.config[key]))
-                
-    def __setattr__(self, key, value):
-        """Prevent directly setting attributes
-
-        This is to ensure that all configuration options are defined
-        directly through the dictionary or YAML adding methods. This
-        limitation could be lifted in the future.
-        """
-        if self.__isFrozen and not hasattr(self, key):
-            raise TypeError(textwrap.fill(textwrap.dedent("""
-            Directly adding new attributes to a Configuration 
-            object using dot notation is not supported. Please use 
-            the update_config() method""")))
-        object.__setattr__(self, key, value)
-
-                
     def update_config(self, new_config):
         self._thaw()
         if isinstance(new_config, dict):
             self.update_from_dict(new_config)
-        else:
+        elif splitext(new_config)[1]=='.yaml':
             self.update_from_yaml(new_config)
+        else:
+            raise TypeError(textwrap.fill(textwrap.dedent("""
+            Please pass either a dictionary or the path to a YAML file 
+            with a .yaml extension as an argument to Config(). Other
+            configuration formats are not supported.
+            """)))
         self._update_state()
         self._freeze()
+
+    def dump_config(self, path=None):
+        """Print or save YAML version of config file
         
-    def update_from_yaml(self, new_config):
-        """Update the stored configuration
-
         kwargs:
-        * new_config: path to YAML file
-
-        If new_config includes a key/value pair already included in the
-        existing config, the existing config will be overwritten with
-        the new value. If new_config includes new key/value pairs, they
-        will be added to the existing config.
+        * path (default None):  if non-null, path to which to save output
         """
-        new_config = yaml.load(open(new_config, 'r'), Loader = yaml.SafeLoader)
-        self.config.update(new_config)
-        self.__dict__.update(self.config)
-        
-    def update_from_dict(self, new_config:dict):
-        """Update the stored configuration
-
-        kwargs:
-        * new_config: python dict
-
-        If new_config includes a key/value pair already included in the
-        existing config, the existing config will be overwritten with
-        the new value. If new_config includes new key/value pairs, they
-        will be added to the existing config.
-        """
-        self.config.update(new_config)    
-        self.__dict__.update(new_config)
+        if path:
+            stream = open(path, 'w')
+            yaml.dump(self.config, stream)
+        print(yaml.dump(self.config, Dumper=yaml.SafeDumper))
