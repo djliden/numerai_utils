333e04cbe6d014457f841406fc773e7ca0d4f74d
dliden <dliden@protonmail.com>
2021-03-17 22:05:49 -0700

    Update Config class for access by dot notation
    Major refactor allows for access to nested configuration items by dot
    notation. For example, a config of the format `{'a':{'b':{'c':1}}}`
    could now be accessed (in Config() instance A) via A.a.b.c.
    
    Given that the config system is the main point of contact between the
    training loop and the user, it is important that the user be able to
    straightforwardly define access to config variables in the training
    loop. In other words, no messing with nested dictionaries. All config
    options can be accessed through dot notation.
    
    Lastly, config is maintained as a dictionary called 'config.' This can
    always be accessed with A.config.

--- src/config/config.py
+++ src/config/config.py
@@ -1,65 +1,17 @@
-from os import PathLike
-from fastcore.basics import AttrDict
 import yaml
+import textwrap
 
-"""
-An important note: At this phase, the classes contained here muddy the
-relationship between the class instance's __dict__ and the stored
-configuration dictionaries (e.g. Configuration.config). This could cause
-problems if one were to try to *update* the configuration by defining a
-class attribute, e.g. Configuration.Model.lr = 0.1. I see the most potential for this to be problematic in the log output -- anything added in this way would
-not be included in the .config or .log dictionary and thus would not be
-included in the output. I hope to find a good way to make both approaches
-work, but in the meantime, to be safe, it is better to *add* to the config
-only using the functions designed for that purpose. Accessing parts of the
-config through class attributes should currently be acceptable, though.
-
-Maybe something from here would help https://docs.python.org/3/reference/datamodel.html?emulating-container-types#emulating-container-types
-
-Or here https://stackoverflow.com/questions/38034377/object-like-attribute-access-for-nested-dictionary
-
-From SE:
-
-class AttrDict(dict):
-"" Dictionary subclass whose entries can be accessed by attributes
-    (as well as normally).
-''
-def __init__(self, *args, **kwargs):
-    def from_nested_dict(data):
-        "" Construct nested AttrDicts from nested dictionaries. ""
-        if not isinstance(data, dict):
-            return data
+class Config():
+    __isFrozen = True
+    def __init__(self, config):
+        self._thaw()
+        self.config = dict()
+        if isinstance(config, dict):
+            self.update_from_dict(config)
         else:
-            return AttrDict({key: from_nested_dict(data[key])
-                                for key in data})
-
-    super(AttrDict, self).__init__(*args, **kwargs)
-    self.__dict__ = self
-
-    for key in self.keys():
-        self[key] = from_nested_dict(self[key])
-
-Or I can just copy YACS https://github.com/rbgirshick/yacs/blob/32d5e4ac300eca6cd3b839097dde39c4017a1070/yacs/config.py#L112
-
-The challenge is with class attribute <-> dict interoperability.
-
-"""
-
-
-class Container(dict):
-    """A parent class for configuration and logging classes"""
-    def __init__(self, *args, **kwargs):
-        super(Container, self).__init__(*args, **kwargs)
-        self.__dict__ = self
-        
-    @staticmethod
-    def from_nested_dicts(data):
-        """ Construct nested AttrDicts from nested dictionaries."""
-        if not isinstance(data, dict):
-            return data
-        else:
-            return Container({key: Container.from_nested_dicts(data[key])
-                                for key in data})
+            self.update_from_yaml(config)
+        self._update_state()
+        self._freeze()
         
     def _freeze(self):
         """Prevent changes to the __dict__"""
@@ -68,29 +20,12 @@ class Container(dict):
     def _thaw(self):
         """Allow changes to the __dict__"""
         self.__isFrozen = False
-
     
-class Configuration(Container):
-    """Class for accessing and updating configuration files
-    
-        kwargs:
-        * config: path to an initial configuration file
-        """
-    __isFrozen = False
-    
-    def __init__(self, config = None):
-        super().__init__()
-        self.config = dict()
-        if config:
-            config = yaml.load(open(config, 'r'), Loader = yaml.SafeLoader)
-            self.config.update(config)
-        new_dict = self.from_nested_dicts(self.config)
-        #self.__dict__.update(self.config)
-        self.__dict__ = new_dict
-        self._freeze(self)
-        
-        
-
+    def _update_state(self):
+        for key in self.config.keys():
+            if isinstance(self.config[key], dict):
+                self.__setattr__(key, Config(self.config[key]))
+                
     def __setattr__(self, key, value):
         """Prevent directly setting attributes
 
@@ -99,13 +34,21 @@ class Configuration(Container):
         limitation could be lifted in the future.
         """
         if self.__isFrozen and not hasattr(self, key):
-            raise TypeError(("Directly adding new attributes to a Configuration "
-                             "object using dot notation is not supported. Please use "
-                             "th update_from_yaml or update_from_dict methods"))
+            raise TypeError(textwrap.fill(textwrap.dedent("""
+            Directly adding new attributes to a Configuration 
+            object using dot notation is not supported. Please use 
+            the update_config() method""")))
         object.__setattr__(self, key, value)
 
-
-    
+                
+    def update_config(self, new_config):
+        self._thaw()
+        if isinstance(new_config, dict):
+            self.update_from_dict(new_config)
+        else:
+            self.update_from_yaml(new_config)
+        self._update_state()
+        self._freeze()
         
     def update_from_yaml(self, new_config):
         """Update the stored configuration
@@ -118,11 +61,9 @@ class Configuration(Container):
         the new value. If new_config includes new key/value pairs, they
         will be added to the existing config.
         """
-        self._thaw()
         new_config = yaml.load(open(new_config, 'r'), Loader = yaml.SafeLoader)
         self.config.update(new_config)
         self.__dict__.update(self.config)
-        self._freeze()
         
     def update_from_dict(self, new_config:dict):
         """Update the stored configuration
@@ -135,44 +76,5 @@ class Configuration(Container):
         the new value. If new_config includes new key/value pairs, they
         will be added to the existing config.
         """
-        self._thaw()
         self.config.update(new_config)    
-        self.__dict__.update(self.config)
-        self._freeze()
-
-    def print_config(self):
-        print(yaml.dump(self.config))
-
-    
-class Log:
-    """Class for logging and returning output of model runs
-
-    kwargs:
-    * include_config: whether the configuration should be included in
-      the log output
-    * config (dict): the configuration to be included, if include_config
-      is True. Note that this argument expects a dictionary, so from an
-      instance of the Configuration class, pass Configuration.config to
-      the log class.
-    """
-    def __init__(self, config:dict, incude_config:bool = True):
-        self.log = dict()
-        if include_config:
-            self.config = config
-            self.log.update(self.config)
-        self.__dict__.update(self.log)
-    
-    
-    def update_from_dict(self, new_log:dict):
-        """Update the stored configuration
-
-        kwargs:
-        * new_log: python dict
-
-        If new_log includes a key/value pair already included in the
-        existing log, the existing log will be overwritten with
-        the new value. If new_log includes new key/value pairs, they
-        will be added to the existing log.
-        """
-        self.log.update(new_log)    
-        self.__dict__.update(self.log)
+        self.__dict__.update(new_config)
